34a30615b1a4ab41ae1d1cd7117f1f8ba9155236
Jace <54625750+Jacekun@users.noreply.github.com>
2022-04-18 09:48:33 +0800

    [Bugfix] Fix duplicate quality on source list on sflix (#963)

--- app/src/main/java/com/lagradost/cloudstream3/movieproviders/SflixProvider.kt
+++ app/src/main/java/com/lagradost/cloudstream3/movieproviders/SflixProvider.kt
@@ -605,14 +605,12 @@ open class SflixProvider : MainAPI() {
                     M3u8Helper().m3u8Generation(M3u8Helper.M3u8Stream(this.file, null), true)
                         .map { stream ->
                             //println("stream: ${stream.quality} at ${stream.streamUrl}")
-                            val qualityString = if ((stream.quality ?: 0) == 0) label
-                                ?: "" else "${stream.quality}p"
                             ExtractorLink(
                                 caller.name,
-                                "${caller.name} $qualityString $name",
+                                "${caller.name} $name",
                                 stream.streamUrl,
                                 caller.mainUrl,
-                                getQualityFromName(stream.quality.toString()),
+                                getQualityFromName(stream.quality?.toString()),
                                 true,
                                 extractorData = extractorData
                             )
