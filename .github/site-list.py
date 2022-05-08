6f05154f50fbb6cd2fc3205217da72cf7580b759
Jace <54625750+Jacekun@users.noreply.github.com>
2022-05-08 20:57:30 +0800

    [Feature] Display language on github page site list. (#1038)
    * add language to gh page
    
    * display language on page
    
    * fixes
    
    * log for debugging
    
    * handle error on parsing language
    
    * minor change
    
    * rename some providers for consistency on naming conventions.
    
    * fixes on site-list generator script:
    - fix key name, use classname from content. defaults to filename.
    - fix provider display name, use name from content. defaults to filename.
    - only build from animeproviders and movieproviders path. prevents unnecessary objects populating json file.

--- .github/site-list.py
+++ .github/site-list.py
@@ -6,42 +6,53 @@ from json import dump, load
 from typing import List, Dict
 
 # Globals
-URL_REGEX = compile(
-    "override\sva[lr]\smainUrl[^\"']+[\"'](https?://[a-zA-Z0-9\.-]+)[\"']")
-NAME_REGEX = compile("([A-Za-z0-9]+)(?:.kt)$")
 JSON_PATH = "docs/providers.json"
-GLOB = "app/src/main/java/com/lagradost/cloudstream3/*providers/*Provider.kt"
+GLOB_ANIME = "app/src/main/java/com/lagradost/cloudstream3/animeproviders/*Provider.kt"
+GLOB_MOVIE = "app/src/main/java/com/lagradost/cloudstream3/movieproviders/*Provider.kt"
+URL_REGEX = compile("override\sva[lr]\smainUrl[^\"']+[\"'](https?://[a-zA-Z0-9\.-]+)[\"']")
+FILENAME_REGEX = compile("([A-Za-z0-9]+)(?:.kt)$")
+PROVIDER_CLASSNAME_REGEX = compile("(?<=class\s)([a-zA-Z]+)(?=\s:\sMainAPI\(\))")
+NAME_REGEX = compile("override\sva[lr]\sname[^\"']+[\"']([a-zA-Z-.\s]+)")
+LANG_REGEX = compile("override\sva[lr]\slang[^\"']+[\"']([a-zA-Z]+)")
 
 old_sites: Dict[str, Dict] = load(open(JSON_PATH, "r", encoding="utf-8"))
 sites: Dict[str, Dict] = {}
 
+animelist = glob(GLOB_ANIME)
+movielist = glob(GLOB_MOVIE)
+allProvidersList = animelist + movielist
+
 # parse all *Provider.kt files
-for path in glob(GLOB):
+for path in allProvidersList:
     with open(path, "r", encoding='utf-8') as file:
         try:
             site_text: str = file.read()
-            name: str = findall(NAME_REGEX, path)[0]
+            filename: str = findall(FILENAME_REGEX, path)[0]
+            name: str = [*findall(PROVIDER_CLASSNAME_REGEX, site_text), filename][0]
             provider_url: str = [*findall(URL_REGEX, site_text), ""][0]
+            lang: str = [*findall(LANG_REGEX, site_text), "en"][0]
 
             if name in old_sites.keys():  # if already in previous list use old status and name
                 sites[name] = {
                     "name": old_sites[name]['name'],
                     "url": provider_url if provider_url else old_sites[name]['url'],
-                    "status": old_sites[name]['status']
+                    "status": old_sites[name]['status'],
+                    "language": lang
                 }
             else: # if not in previous list add with new data
-                display_name = name
+                display_name: str = [*findall(NAME_REGEX, site_text), name][0]
                 if display_name.endswith("Provider"):
                     display_name = display_name[:-len("Provider")]
                 sites[name] = {
                     "name": display_name,
                     "url": provider_url if provider_url else "",
-                    "status": 1
+                    "status": 1,
+                    "language": lang
                 }
 
         except Exception as ex:
-            print("{0}: {1}".format(path, ex))
-            
+            print("Error => {0}: {1}".format(path, ex))
+
 # add sites from old_sites that are missing in new list
 for name in old_sites.keys():
     if name not in sites.keys():
--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,6 @@
 const count = document.getElementById("count")
-const mainContainer = document.getElementById("siteList");
+const mainContainer = document.getElementById("siteList")
+const regionNamesInEnglish = new Intl.DisplayNames(['en'], { type: 'language' });
 fetch("providers.json" + "?v=" + Date.now())
     .then(r => r.json())
     .then(function (data) {
@@ -10,6 +11,7 @@ fetch("providers.json" + "?v=" + Date.now())
                 if (value.url == "NONE") { continue; }
 
                 var _status = value.status
+                var _lang = value.language
 
                 var node = document.createElement("tr");
                 node.classList.add("row");
@@ -17,6 +19,17 @@ fetch("providers.json" + "?v=" + Date.now())
                 var _a = document.createElement("a");
                 _a.setAttribute('href', value.url);
                 _a.innerHTML = value.name
+
+                var _langEl = document.createElement("sup");
+                var _langName = "Unknown";
+                try {
+                    _langName = regionNamesInEnglish.of(_lang);
+                } catch (langerr) {
+                    _langName = "Unknown";
+                    console.log("Error on parsing language code => " + langerr);
+                }
+                _langEl.innerHTML = " " + _langName
+                _a.appendChild(_langEl);
 
                 var _statusText = "Unknown";
                 var _buttonText = "yellow";
