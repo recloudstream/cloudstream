1f56d0c12edf043ca8bd15a5cd899a7503c5b2d4
LagradOst <11805592+LagradOst@users.noreply.github.com>
2022-05-26 20:08:32 +0200

    fixed selecting lang 1 instead of lang

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
@@ -480,7 +480,8 @@ class GeneratorPlayer : FullScreenPlayer() {
 
         if (settings)
             subtitles.firstOrNull { sub ->
-                sub.name.replace(Regex("[^A-Za-z]")," ").startsWith("$lang ")
+                val t = sub.name.replace(Regex("[^A-Za-z]"), " ").trim()
+                t == lang || t.startsWith("$lang ")
                         || sub.name.trim() == langCode
             }?.let { sub ->
                 return sub
