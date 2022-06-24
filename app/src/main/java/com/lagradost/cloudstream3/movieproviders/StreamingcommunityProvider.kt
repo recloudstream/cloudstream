5abbeb9ecad003ab8fa2af0e7b231e7a1a0b17a3
antonydp <38143733+antonydp@users.noreply.github.com>
2022-06-25 00:19:52 +0200

    Streamingcommunity Domain fix (#1214)
    * bug fixes and add cast
    
    * fixed Altadefinizione search
    
    * fixed Altadefinizione search
    
    * Add trailer to italians providers
    
    * Update StreamingcommunityProvider.kt
    
    Fix provider domain
    
    Co-authored-by: Osten <11805592+LagradOst@users.noreply.github.com>

--- app/src/main/java/com/lagradost/cloudstream3/movieproviders/StreamingcommunityProvider.kt
+++ app/src/main/java/com/lagradost/cloudstream3/movieproviders/StreamingcommunityProvider.kt
@@ -128,7 +128,7 @@ data class TrailerElement(
 
 class StreamingcommunityProvider : MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://streamingcommunity.press"
+    override var mainUrl = "https://streamingcommunity.business"
     override var name = "Streamingcommunity"
     override val hasMainPage = true
     override val hasChromecastSupport = true
