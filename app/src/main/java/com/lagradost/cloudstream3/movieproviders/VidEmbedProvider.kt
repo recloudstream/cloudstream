53a6a9b1a4eb51ec4bf29dbf759461b4b7ca2a2a
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-07-20 10:33:48 +0530

    Update VidEmbedProvider.kt (#1314)

--- app/src/main/java/com/lagradost/cloudstream3/movieproviders/VidEmbedProvider.kt
+++ app/src/main/java/com/lagradost/cloudstream3/movieproviders/VidEmbedProvider.kt
@@ -7,7 +7,7 @@ import com.lagradost.cloudstream3.TvType
  */
 class VidEmbedProvider : VidstreamProviderTemplate() {
     // mainUrl is good to have as a holder for the url to make future changes easier.
-    override var mainUrl = "https://vidembed.cc"
+    override var mainUrl = "https://membed.net"
 
     // name is for how the provider will be named which is visible in the UI, no real rules for this.
     override var name = "VidEmbed"
