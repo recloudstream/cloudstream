654602b933a2d33b4f8421eb02b72371250d3e44
Jace <54625750+Jacekun@users.noreply.github.com>
2022-11-19 13:03:51 +0800

    [Feature] Get duration from string in format of  '00 hr 00 min 00 sec', in any combination

--- app/src/main/java/com/lagradost/cloudstream3/MainAPI.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainAPI.kt
@@ -1153,6 +1153,27 @@ fun getDurationFromString(input: String?): Int? {
             return values[1].toIntOrNull()
         }
     }
+    Regex("(\\s\\d+\\shr)|(\\s\\d+\\shour)|(\\s\\d+\\smin)|(\\s\\d+\\ssec)").findAll(input).let { values ->
+        var seconds = 0
+        values.forEach {
+            val time_text = it.value
+            if (time_text.isNotBlank()) {
+                val time = time_text.filter { s -> s.isDigit() }.trim().toInt()
+                val scale = time_text.filter { s -> !s.isDigit() }.trim()
+                //println("Scale: $scale")
+                val timeval = when (scale) {
+                    "hr", "hour" -> time * 60 * 60
+                    "min" -> time * 60
+                    "sec" -> time
+                    else -> 0
+                }
+                seconds += timeval
+            }
+        }
+        if (seconds > 0) {
+            return seconds / 60
+        }
+    }
     return null
 }
 
