30ff7d988f8e260d4514291e02eaa860da5cb3d4
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-02 11:25:12 +0000

    fix

--- app/src/main/java/com/lagradost/cloudstream3/utils/InAppUpdater.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/InAppUpdater.kt
@@ -357,31 +357,31 @@ class InAppUpdater {
             }
             return false
         }
-    }
-
-    fun isMiUi(): Boolean {
-        return !TextUtils.isEmpty(getSystemProperty("ro.miui.ui.version.name"))
-    }
-    
-    fun getSystemProperty(propName: String): String? {
-        var line: String? = null
-        var input: BufferedReader? = null
-        try {
-            val p = Runtime.getRuntime().exec("getprop $propName")
-            input = BufferedReader(InputStreamReader(p.inputStream), 1024)
-            line = input.readLine()
-            input.close()
-        } catch (ex: IOException) {
-            return null
-        } finally {
-            if (input != null) {
-                try {
-                    input.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
+        
+        fun isMiUi(): Boolean {
+            return !TextUtils.isEmpty(getSystemProperty("ro.miui.ui.version.name"))
+        }
+        
+        fun getSystemProperty(propName: String): String? {
+            var line: String? = null
+            var input: BufferedReader? = null
+            try {
+                val p = Runtime.getRuntime().exec("getprop $propName")
+                input = BufferedReader(InputStreamReader(p.inputStream), 1024)
+                line = input.readLine()
+                input.close()
+            } catch (ex: IOException) {
+                return null
+            } finally {
+                if (input != null) {
+                    try {
+                        input.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
                 }
             }
+            return line
         }
-        return line
     }
 }
