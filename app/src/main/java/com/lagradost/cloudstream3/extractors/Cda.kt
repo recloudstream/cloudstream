cd2273de6ca22971c0e73ab82379101273abf9cb
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-02 11:41:17 +0000

    oops

--- app/src/main/java/com/lagradost/cloudstream3/extractors/Cda.kt
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/Cda.kt
@@ -62,7 +62,7 @@ open class Cda: ExtractorApi() {
             .replace("_Q5", "")
             .replace("_IKSDE", "")
         a = URLDecoder.decode(a, "UTF-8")
-        a = a.split("").map { char ->
+        a = a.map { char ->
             if (32 < char.toInt() && char.toInt() < 127) {
                 return@map String.format("%c", 33 + (char.toInt() + 14) % 94)
             } else {
