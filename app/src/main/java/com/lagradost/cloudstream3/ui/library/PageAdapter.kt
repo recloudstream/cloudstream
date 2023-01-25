9bc90438eca76cf5afa84c2cba46fb74b653d57a
reduplicated <110570621+reduplicated@users.noreply.github.com>
2023-01-25 16:54:57 +0100

    colors go brrr

--- app/src/main/java/com/lagradost/cloudstream3/ui/library/PageAdapter.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/library/PageAdapter.kt
@@ -1,12 +1,17 @@
 package com.lagradost.cloudstream3.ui.library
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.lagradost.cloudstream3.AcraApplication
 import com.lagradost.cloudstream3.R
 import com.lagradost.cloudstream3.syncproviders.SyncAPI
 import com.lagradost.cloudstream3.ui.AutofitRecyclerView
@@ -16,6 +21,7 @@ import com.lagradost.cloudstream3.utils.AppUtils
 import com.lagradost.cloudstream3.utils.UIHelper.toPx
 import kotlinx.android.synthetic.main.search_result_grid_expanded.view.*
 import kotlin.math.roundToInt
+
 
 class PageAdapter(
     override val items: MutableList<SyncAPI.LibraryItem>,
@@ -39,6 +45,18 @@ class PageAdapter(
         }
     }
 
+    private fun isDark(color: Int): Boolean {
+        return ColorUtils.calculateLuminance(color) < 0.5
+    }
+
+    fun getDifferentColor(color: Int, ratio : Float = 0.7f) : Int {
+        return if(isDark(color)) {
+            ColorUtils.blendARGB(color, Color.WHITE, ratio)
+        } else{
+            ColorUtils.blendARGB(color, Color.BLACK, ratio)
+        }
+    }
+
     inner class LibraryItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val cardView: ImageView = itemView.imageView
 
@@ -47,11 +65,35 @@ class PageAdapter(
             if (compactView) 80.toPx else (resView.itemWidth / 0.68).roundToInt()
 
         fun bind(item: SyncAPI.LibraryItem, position: Int) {
+            /** https://stackoverflow.com/questions/8817522/how-to-get-color-code-of-image-view */
+
             SearchResultBuilder.bind(
                 this@PageAdapter.clickCallback,
                 item,
                 position,
                 itemView,
+                colorCallback = { palette ->
+                    AcraApplication.context?.let { ctx ->
+                        val defColor = ContextCompat.getColor(ctx,R.color.ratingColorBg)
+                        var bg = palette.getDarkVibrantColor(defColor)
+                        if (bg == defColor) {
+                            bg = palette.getDarkMutedColor(defColor)
+                        }
+                        if (bg == defColor) {
+                            bg = palette.getVibrantColor(defColor)
+                        }
+
+                        val fg = getDifferentColor(bg)//palette.getVibrantColor(ContextCompat.getColor(ctx,R.color.ratingColor))
+                        itemView.text_rating.apply {
+                            setTextColor(ColorStateList.valueOf(fg))
+                        }
+                        itemView.text_rating_holder?.backgroundTintList =ColorStateList.valueOf(bg)
+                        itemView.watchProgress?.apply {
+                            progressTintList = ColorStateList.valueOf(fg)
+                            progressBackgroundTintList = ColorStateList.valueOf(bg)
+                        }
+                    }
+                }
             )
 
             // See searchAdaptor for this, it basically fixes the height
@@ -74,9 +116,9 @@ class PageAdapter(
             itemView.imageText.text = item.name
 
             val showRating = (item.personalRating ?: 0) != 0
-            itemView.text_rating.isVisible = showRating
+            itemView.text_rating_holder.isVisible = showRating
             if (showRating) {
-                itemView.text_rating.text = item.personalRating.toString()
+                itemView.text_rating.text = "★ ${item.personalRating.toString()}"
             }
         }
     }
