ee4517093f4be8a4c1cb3d7bac9b4865db59d067
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-02-11 14:12:31 +0000

    lets use dynamic colors instead

--- app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
@@ -2,7 +2,6 @@ package com.lagradost.cloudstream3
 
 import android.content.ComponentName
 import android.content.Context
-import android.graphics.Color;
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.content.res.Configuration
@@ -17,6 +16,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -731,9 +731,9 @@ class MainActivity : AppCompatActivity(), ColorPickerDialogListener {
                         .setAction("Revert", v -> {
                             setKey("jsdelivr_proxy_key", false)
                         })
-                        .setBackgroundTint(0xffffbb33)
-                        .setTextColor(Color.BLACK)
-                        .setActionTextColor(Color.BLACK)
+                        .setBackgroundTint(ContextCompat.getColor(this@MainActivity, R.color.primaryGrayBackground))
+                        .setTextColor(ContextCompat.getColor(this@MainActivity, R.color.textColor))
+                        .setActionTextColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
                         .show()
                 }
 
