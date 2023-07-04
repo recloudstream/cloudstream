5a5640b3440dcb5ef1c671cf3eb3b6e052deb805
Thorodinson1 <maniwertyu123@gmail.com>
2023-07-04 18:28:13 +0530

    added streamoupload extractor

---
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/StreamoUpload.kt
@@ -0,0 +1,42 @@
+package com.lagradost.cloudstream3.extractors
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.M3u8Helper
+
+class StreamoUpload1 : StreamoUpload() {
+    override val mainUrl = "https://streamoupload.xyz"
+}
+
+open class StreamoUpload : ExtractorApi() {
+    override val name = "StreamoUpload"
+    override val mainUrl = "https://streamoupload.xyz"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
+        val sources = mutableListOf<ExtractorLink>()
+        app.get(url, referer = referer).document.select("script").map { script ->
+            if (script.data().contains("jwplayer(\"vplayer\").setup(")) {
+                val data = script.data().substringAfter("sources: [")
+                    .substringBefore("],").replace("file", "\"file\"").trim()
+                tryParseJson<File>(data)?.let {
+                    M3u8Helper.generateM3u8(
+                        name,
+                        it.file,
+                        "$mainUrl/",
+                    ).forEach { m3uData -> sources.add(m3uData) }
+                }
+            }
+        }
+        return sources
+    }
+
+    private data class File(
+        @JsonProperty("file") val file: String,
+    )
+
+
+}
