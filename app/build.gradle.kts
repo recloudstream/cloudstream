d277d8a9aabce833b6384948716a5c3d8c680807
LagradOst <11805592+LagradOst@users.noreply.github.com>
2023-10-10 16:56:30 +0200

    bump upstream

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,16 +50,16 @@ android {
         }
     }
 
-    compileSdk = 33
+    compileSdk = 34
     buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "com.lagradost.cloudstream3"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
 
         versionCode = 62
-        versionName = "4.2.0"
+        versionName = "4.2.1"
 
         resValue("string", "app_version", "${defaultConfig.versionName}${versionNameSuffix ?: ""}")
         resValue("string", "commit_hash", "git rev-parse --short HEAD".execute() ?: "")
@@ -154,18 +154,18 @@ repositories {
 dependencies {
     implementation("com.google.android.mediahome:video:1.0.0")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
-    testImplementation("org.json:json:20180813")
+    testImplementation("org.json:json:20230618")
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1") // need target 32 for 1.5.0
 
     // dont change this to 1.6.0 it looks ugly af
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -173,9 +173,9 @@ dependencies {
 
     // implementation("io.karn:khttp-android:0.1.2") //okhttp instead
     // implementation("org.jsoup:jsoup:1.13.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 
-    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 
     implementation("com.github.bumptech.glide:glide:4.13.1")
     kapt("com.github.bumptech.glide:compiler:4.13.1")
@@ -200,14 +200,14 @@ dependencies {
     implementation("com.github.recloudstream:media-ffmpeg:1.1.0")
 
     // Bug reports
-    implementation("ch.acra:acra-core:5.11.0")
-    implementation("ch.acra:acra-toast:5.11.0")
+    implementation("ch.acra:acra-core:5.11.2")
+    implementation("ch.acra:acra-toast:5.11.2")
 
-    compileOnly("com.google.auto.service:auto-service-annotations:1.0")
+    compileOnly("com.google.auto.service:auto-service-annotations:1.1.1")
     //either for java sources:
-    annotationProcessor("com.google.auto.service:auto-service:1.0")
+    annotationProcessor("com.google.auto.service:auto-service:1.1.1")
     //or for kotlin sources (requires kapt gradle plugin):
-    kapt("com.google.auto.service:auto-service:1.0")
+    kapt("com.google.auto.service:auto-service:1.1.1")
 
     // subtitle color picker
     implementation("com.jaredrummler:colorpicker:1.1.0")
@@ -229,7 +229,7 @@ dependencies {
     // implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.9.1")
     implementation("com.github.Blatzar:NiceHttp:0.4.3")
     // To fix SSL fuckery on android 9
-    implementation("org.conscrypt:conscrypt-android:2.2.1")
+    implementation("org.conscrypt:conscrypt-android:2.5.2")
     // Util to skip the URI file fuckery 🙏
     implementation("com.github.LagradOst:SafeFile:0.0.5")
 
