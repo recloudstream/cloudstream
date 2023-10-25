d8cf969faf33414aaa5bfca9f654a9393f3ce247
Manna Huizar <38449294+Jubes2011@users.noreply.github.com>
2023-10-25 11:49:48 -0500

    Create IceAndFire.kt

---
+++ app/src/main/java/com/lagradost/cloudstream3/metaproviders/IceAndFire.kt
@@ -0,0 +1,53 @@
+
+	"url": "https://anapioficeandfire.com/api/characters/583",
+	"name": "Jon Snow",
+	"gender": "Male",
+	"culture": "Northmen",
+	"born": "In 283 AC",
+	"died": "",
+	"titles": [
+		"Lord Commander of the Night's Watch"
+	],
+	"aliases": [
+		"Lord Snow",
+		"Ned Stark's Bastard",
+		"The Snow of Winterfell",
+		"The Crow-Come-Over",
+		"The 998th Lord Commander of the Night's Watch",
+		"The Bastard of Winterfell",
+		"The Black Bastard of the Wall",
+		"Lord Crow"
+	],
+	"father": "",
+	"mother": "",
+	"spouse": "",
+	"allegiances": [
+		"https://anapioficeandfire.com/api/houses/362"
+	],
+	"books": [
+		"https://anapioficeandfire.com/api/books/5"
+	],
+	"povBooks": [
+		"https://anapioficeandfire.com/api/books/1",
+		"https://anapioficeandfire.com/api/books/2",
+		"https://anapioficeandfire.com/api/books/3",
+		"https://anapioficeandfire.com/api/books/8"
+	],
+	"tvSeries": [
+		"Season 1",
+		"Season 2",
+		"Season 3",
+		"Season 4",
+		"Season 5",
+		"Season 6"
+	],
+	"playedBy": [
+		"Kit Harington"
+	]
+}
+GET STARTED
+The world's greatest source for quantified and structured data from the universe of Ice and Fire (and the HBO series Game of Thrones).
+
+The API is open source! Contribute directly to the API by writing code, documentation or posting bugs!
+
+All data is accessible through our HTTP API with SDKs in a bunch of different languages. Check out the documentation to get started.
