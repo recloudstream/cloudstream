355e090f272d540cdf39d226540b3a2fee78c47b
KingLucius <escuro.anjo@gmail.com>
2023-10-31 14:48:17 +0200

    Fix settings top bar on TV

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsAccount.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsAccount.kt
@@ -30,6 +30,7 @@ import com.lagradost.cloudstream3.syncproviders.OAuth2API
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.getPref
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.isTvSettings
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.setPaddingBottom
+import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.setToolBarScrollFlags
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.setUpToolbar
 import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
 import com.lagradost.cloudstream3.utils.UIHelper.dismissSafe
@@ -248,6 +249,7 @@ class SettingsAccount : PreferenceFragmentCompat() {
         super.onViewCreated(view, savedInstanceState)
         setUpToolbar(R.string.category_account)
         setPaddingBottom()
+        setToolBarScrollFlags()
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
