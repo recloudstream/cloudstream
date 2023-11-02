735eb37250a972ed7f9cbd0c7563957a53d4e854
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-11-02 23:48:25 +0530

    bump navigation lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -164,8 +164,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
