3386f5e9945c30ebefe4b8ff4e375438f2d4683f
CranberrySoup <142951702+CranberrySoup@users.noreply.github.com>
2023-11-15 01:17:28 +0000

    Update SettingsGeneral.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsGeneral.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsGeneral.kt
@@ -55,7 +55,7 @@ fun getCurrentLocale(context: Context): String {
 // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes leave blank for auto
 val appLanguages = arrayListOf(
     /* begin language list */
-    Triple("", "ajp", "ajp"),
+    Triple("", "عربي شامي", "ajp"),
     Triple("", "አማርኛ", "am"),
     Triple("", "العربية", "ar"),
     Triple("", "ars", "ars"),
