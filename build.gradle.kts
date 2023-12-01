52c743b545ebef727a7afe88dd0c077d7269fa03
IndusAryan <serena01011975@gmail.com>
2023-12-01 16:17:13 +0530

    upgrade android gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
 
