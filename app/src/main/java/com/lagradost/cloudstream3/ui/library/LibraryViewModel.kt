530619c8d0f9546de595cfdbe99bf44d468e0e56
Funny-Pen-7005 <153119899+Funny-Pen-7005@users.noreply.github.com>
2023-12-09 21:08:39 +0530

    fixes (library): reverted and updated currentPage logic (#802)
    Co-authored-by: Funny-Pen-7005 <Funny-Pen-7005>

--- app/src/main/java/com/lagradost/cloudstream3/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/library/LibraryViewModel.kt
@@ -28,15 +28,7 @@ enum class ListSorting(@StringRes val stringRes: Int) {
 const val LAST_SYNC_API_KEY = "last_sync_api"
 
 class LibraryViewModel : ViewModel() {
-    private val tabPositionLiveData = MutableLiveData<Int>()
-
-    fun setTabPosition(position: Int) {
-        tabPositionLiveData.value = position
-    }
-
-    fun getTabPosition(): LiveData<Int> {
-        return tabPositionLiveData
-    }
+    var currentPage: Int = 0
     
     private val _pages: MutableLiveData<Resource<List<SyncAPI.Page>>> = MutableLiveData(null)
     val pages: LiveData<Resource<List<SyncAPI.Page>>> = _pages
