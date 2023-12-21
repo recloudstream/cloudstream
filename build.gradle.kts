ed8378687d40295e0db03af8ef616816b89eaf4e
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-12-21 20:57:12 +0530

    Update kotlin gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
 
         // NOTE: Do not place your application dependencies here; they belong
