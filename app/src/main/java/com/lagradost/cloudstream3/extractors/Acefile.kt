f68750852107d375c69f09dda41c636acf7d3e67
Sofie <117321707+Sofie99@users.noreply.github.com>
2024-01-05 23:08:48 +0700

    Extractors: fix acefile, pixeldrain & vidplay tracks (#854)
    Co-authored-by: ghost <ghost@gmail.com>

--- app/src/main/java/com/lagradost/cloudstream3/extractors/Acefile.kt
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/Acefile.kt
@@ -1,5 +1,6 @@
 package com.lagradost.cloudstream3.extractors
 
+import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.base64Decode
 import com.lagradost.cloudstream3.utils.*
@@ -9,31 +10,34 @@ open class Acefile : ExtractorApi() {
     override val mainUrl = "https://acefile.co"
     override val requiresReferer = false
 
-    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
-        val sources = mutableListOf<ExtractorLink>()
-        app.get(url).document.select("script").map { script ->
-            if (script.data().contains("eval(function(p,a,c,k,e,d)")) {
-                val data = getAndUnpack(script.data())
-                val id = data.substringAfter("{\"id\":\"").substringBefore("\",")
-                val key = data.substringAfter("var nfck=\"").substringBefore("\";")
-                app.get("https://acefile.co/local/$id?key=$key").text.let {
-                    base64Decode(
-                        it.substringAfter("JSON.parse(atob(\"").substringBefore("\"))")
-                    ).let { res ->
-                        sources.add(
-                            ExtractorLink(
-                                name,
-                                name,
-                                res.substringAfter("\"file\":\"").substringBefore("\","),
-                                "$mainUrl/",
-                                Qualities.Unknown.value,
-                            )
-                        )
-                    }
-                }
-            }
-        }
-        return sources
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val script = getAndUnpack(app.get(url).text)
+        val id = script.substringAfter("\"code\":\"").substringBefore("\",")
+        val doc = app.get("https://drive.google.com/uc?id=${base64Decode(id)}&export=download").document
+        val form = doc.select("form#download-form").attr("action")
+        val uc = doc.select("input#uc-download-link").attr("value")
+        val video = app.post(
+            form, data = mapOf(
+                "uc-download-link" to uc
+            )
+        ).url
+
+        callback.invoke(
+            ExtractorLink(
+                this.name,
+                this.name,
+                video,
+                "",
+                Qualities.Unknown.value,
+                INFER_TYPE
+            )
+        )
+
     }
 
 }
