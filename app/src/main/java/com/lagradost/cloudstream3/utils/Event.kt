835387b2449bfcdb71b0fc13313f574e76ab7c7e
CranberrySoup <142951702+CranberrySoup@users.noreply.github.com>
2024-01-18 14:54:39 +0000

    Update Event.kt

--- app/src/main/java/com/lagradost/cloudstream3/utils/Event.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/Event.kt
@@ -1,30 +1,26 @@
 package com.lagradost.cloudstream3.utils
 
-import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-
 class Event<T> {
     private val observers = mutableSetOf<(T) -> Unit>()
 
     val size: Int get() = observers.size
 
     operator fun plusAssign(observer: (T) -> Unit) {
-        observers.add(observer)
+        synchronized(observers) {
+            observers.add(observer)
+        }
     }
 
     operator fun minusAssign(observer: (T) -> Unit) {
-        observers.remove(observer)
+        synchronized(observers) {
+            observers.remove(observer)
+        }
     }
 
-    private val invokeMutex = Mutex()
-
     operator fun invoke(value: T) {
-        ioSafe {
-            invokeMutex.withLock { // Can crash otherwise
-                for (observer in observers)
-                    observer(value)
-            }
+        synchronized(observers) {
+            for (observer in observers)
+                observer(value)
         }
     }
 }
