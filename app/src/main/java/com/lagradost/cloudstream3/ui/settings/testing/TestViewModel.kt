ff6a69ea08077c5bd823631a3d568be98de9d7f2
KingLucius <a.aelfattah5@gmail.com>
2024-02-27 17:03:11 +0200

    Bump Nicehttp & remove toImmutableList

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/testing/TestViewModel.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/testing/TestViewModel.kt
@@ -10,7 +10,6 @@ import com.lagradost.cloudstream3.utils.TestingUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import okhttp3.internal.toImmutableList
 
 class TestViewModel : ViewModel() {
     data class TestProgress(
