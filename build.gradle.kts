4468ce3d800c327d4f82cccbfac09dc9c92c2cbc
firelight <147925818+fire-light42@users.noreply.github.com>
2024-03-22 23:06:05 +0100

    Revert "make cloudstream very superfast boi, fast app startup and navigation …" (#1007)
    This reverts commit faeb71da2cee1d0069acf5e9e3025cd956426b2d.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
@@ -5,9 +6,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -19,12 +23,7 @@ allprojects {
 }
 
 plugins {
-    // these work only in this order, don't change.
     id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
-    id("com.android.test") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("androidx.baselineprofile") version "1.2.2" apply false
-    id("com.android.application") version "8.2.2" apply false
 }
 
 tasks.register<Delete>("clean") {
