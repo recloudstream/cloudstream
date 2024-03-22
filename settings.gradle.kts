04285b6ba24f41c18108fc9eb3ca5f29fbd98dcc
firelight <147925818+fire-light42@users.noreply.github.com>
2024-03-22 23:05:42 +0100

    Revert "make cloudstream very superfast boi, fast app startup and navigation …"
    This reverts commit faeb71da2cee1d0069acf5e9e3025cd956426b2d.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "CloudStream"
 
 include(":app")
-include(":baselineprofile")
