dd5e9ecb3c6733101e41f42256bb11093f8501de
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-03-27 21:29:51 +0530

    Update vtbe.kt

--- app/src/main/java/com/lagradost/cloudstream3/extractors/vtbe.kt
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/vtbe.kt
@@ -5,10 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.JsUnpacker
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.getQualityFromName
+import java.net.URI
+import com.lagradost.cloudstream3.utils.JsUnpacker
+
 
 open class vtbe : ExtractorApi() {
     override var name = "Vtbe"
-    override var mainUrl = "https://vtbe.to/"
+    override var mainUrl = "https://vtbe.to"
     override val requiresReferer = true
 
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
