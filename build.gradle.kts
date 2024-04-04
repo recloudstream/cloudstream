8bea97947ff296c6bb1eddf3664e0b45ce093232
CranberrySoup <142951702+CranberrySoup@users.noreply.github.com>
2024-04-04 17:07:24 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,6 @@ buildscript {
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
         // Universal build config
         classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.15.1")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
     }
 }
 
