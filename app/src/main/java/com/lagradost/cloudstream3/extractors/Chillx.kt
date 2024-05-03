f803219c993bfd4e6911d01d19c20ca64ee5797f
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-05-03 08:11:55 +0530

    Update Chillx.kt

--- app/src/main/java/com/lagradost/cloudstream3/extractors/Chillx.kt
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/Chillx.kt
@@ -41,7 +41,6 @@ open class Chillx : ExtractorApi() {
                 referer = url,
             ).text
         )?.groupValues?.get(1)
-        val key = app.get("https://raw.githubusercontent.com/rushi-chavan/multi-keys/keys/keys.json").parsedSafe<Keys>()?.key?.get(0) ?: throw ErrorLoadingException("Unable to get key")
         val decrypt = cryptoAESHandler(master ?: "",key.toByteArray(), false)?.replace("\\", "") ?: throw ErrorLoadingException("failed to decrypt")
         val source = Regex(""""?file"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
         val subtitles = Regex("""subtitle"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
@@ -83,6 +82,13 @@ open class Chillx : ExtractorApi() {
         return regex.replace(input) {
             it.groupValues[1].toInt(16).toChar().toString()
         }
+    }
+
+    private var key: String? = null
+    val key = fetchKey() ?: throw ErrorLoadingException("Unable to get key")
+
+    private suspend fun fetchKey(): String? {
+    return app.get("https://raw.githubusercontent.com/rushi-chavan/multi-keys/keys/keys.json").parsedSafe()?.key?.get(0)?.also { key = it }
     }
 
     data class Keys(
