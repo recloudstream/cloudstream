0a5399d9b69d013ad9f08e0e3161987d128a8f55
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-05-05 04:30:42 +0530

    Updates and Chillx Extractor Updated (#1065)

--- app/src/main/java/com/lagradost/cloudstream3/extractors/Chillx.kt
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/Chillx.kt
@@ -2,9 +2,7 @@ package com.lagradost.cloudstream3.extractors
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.extractors.helper.*
 import com.lagradost.cloudstream3.extractors.helper.AesHelper.cryptoAESHandler
-import com.lagradost.cloudstream3.utils.AppUtils
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper
@@ -28,30 +26,39 @@ open class Chillx : ExtractorApi() {
     override val name = "Chillx"
     override val mainUrl = "https://chillx.top"
     override val requiresReferer = true
-    private var key: String? = null
 
+    companion object {
+        private var key: String? = null
+
+        suspend fun fetchKey(): String {
+            return if (key != null) {
+                key!!
+            } else {
+                val fetch = app.get("https://raw.githubusercontent.com/rushi-chavan/multi-keys/keys/keys.json").parsedSafe<Keys>()?.key?.get(0) ?: throw ErrorLoadingException("Unable to get key")
+                key = fetch
+                key!!
+            }
+        }
+    }
+
+    @Suppress("NAME_SHADOWING")
     override suspend fun getUrl(
         url: String,
         referer: String?,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val master = Regex("\\s*=\\s*'([^']+)").find(
+        val master = Regex("""JScript[\w+]?\s*=\s*'([^']+)""").find(
             app.get(
                 url,
-                referer = referer ?: "",
-                headers = mapOf(
-                    "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                    "Accept-Language" to "en-US,en;q=0.5",
-                )
+                referer = url,
             ).text
         )?.groupValues?.get(1)
-        val decrypt = cryptoAESHandler(master ?: return, getKey().toByteArray(), false)?.replace("\\", "") ?: throw ErrorLoadingException("failed to decrypt")
-
+        val key = fetchKey()
+        val decrypt = cryptoAESHandler(master ?: "", key.toByteArray(), false)?.replace("\\", "") ?: throw ErrorLoadingException("failed to decrypt")
         val source = Regex(""""?file"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
-
         val subtitles = Regex("""subtitle"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
-        val subtitlePattern = """\[(.*?)\](https?://[^\s,]+)""".toRegex()
+        val subtitlePattern = """\[(.*?)](https?://[^\s,]+)""".toRegex()
         val matches = subtitlePattern.findAll(subtitles ?: "")
         val languageUrlPairs = matches.map { matchResult ->
             val (language, url) = matchResult.destructured
@@ -83,23 +90,18 @@ open class Chillx : ExtractorApi() {
             headers = headers
         ).forEach(callback)
     }
-    
+
     private fun decodeUnicodeEscape(input: String): String {
         val regex = Regex("u([0-9a-fA-F]{4})")
         return regex.replace(input) {
             it.groupValues[1].toInt(16).toChar().toString()
         }
     }
-    
-    suspend fun getKey() = key ?: fetchKey().also { key = it }
 
-    private suspend fun fetchKey(): String {
-        return app.get("https://raw.githubusercontent.com/Sofie99/Resources/main/chillix_key.json").parsed()
-    }
 
-    data class Tracks(
-        @JsonProperty("file") val file: String? = null,
-        @JsonProperty("label") val label: String? = null,
-        @JsonProperty("kind") val kind: String? = null,
+
+    data class Keys(
+        @JsonProperty("chillx") val key: List<String>
     )
+
 }
