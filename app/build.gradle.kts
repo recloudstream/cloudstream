5bdc94ae07fdc045d471de12ebf1df34f9ac7cc0
IndusAryan <serena01011975@gmail.com>
2024-05-15 00:28:45 +0530

    remove comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -219,9 +219,7 @@ dependencies {
     implementation("com.github.rubensousa:previewseekbar-media3:1.1.1.0") // SeekBar Preview
 
     // Extensions & Other Libs
-    implementation("org.mozilla:rhino:1.7.15") /* run JavaScript
-    ^ Don't Bump RhinoJS to 1.7.14,`NoClassDefFoundError` Occurs and Trailers won't play (even with Desugaring)
-     NewPipeExtractor Issue */
+    implementation("org.mozilla:rhino:1.7.15") // run JavaScript
     implementation("me.xdrop:fuzzywuzzy:1.4.0") // Library/Ext Searching with Levenshtein Distance
     implementation("com.github.LagradOst:SafeFile:0.0.6") // To Prevent the URI File Fu*kery
     implementation("org.conscrypt:conscrypt-android:2.5.2") // To Fix SSL Fu*kery on Android 9
