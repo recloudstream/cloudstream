e5c9e96c8347cf31cc7ee25e2490cc70046167c3
firelight <147925818+fire-light42@users.noreply.github.com>
2024-07-04 22:33:21 +0200

    fix filesystem

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainAPI.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainAPI.kt
@@ -29,6 +29,11 @@ import kotlin.math.absoluteValue
  **/
 const val AllLanguagesName = "universal"
 
+const val USER_AGENT =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+
+class ErrorLoadingException(message: String? = null) : Exception(message)
+
 //val baseHeader = mapOf("User-Agent" to USER_AGENT)
 val mapper = JsonMapper.builder().addModule(kotlinModule())
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).build()!!
