7059a819e3e185046d0d22b2a16e6126e78e33bf
b4byhuey <60543438+b4byhuey@users.noreply.github.com>
2024-08-21 11:11:33 +0800

    Update Chillx.kt

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/Chillx.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/Chillx.kt
@@ -1,16 +1,26 @@
 package com.lagradost.cloudstream3.extractors
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.ErrorLoadingException
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.USER_AGENT
+import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.extractors.helper.AesHelper.cryptoAESHandler
+import com.lagradost.cloudstream3.fixTitle
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.M3u8Helper
-import kotlin.run
+import com.lagradost.cloudstream3.utils.INFER_TYPE
+import com.lagradost.cloudstream3.utils.Qualities
+import java.net.URI
 
-class Moviesapi : Chillx() {
-    override val name = "Moviesapi"
-    override val mainUrl = "https://w1.moviesapi.club"
+class Watchx : Chillx() {
+    override val name = "Watchx"
+    override val mainUrl = "https://watchx.top"
+}
+
+class Beastx : Chillx() {
+    override val name = "Beastx"
+    override val mainUrl = "https://beastx.top"
 }
 
 class Bestx : Chillx() {
@@ -18,53 +28,86 @@ class Bestx : Chillx() {
     override val mainUrl = "https://bestx.stream"
 }
 
-class Watchx : Chillx() {
-    override val name = "Watchx"
-    override val mainUrl = "https://watchx.top"
+class Playerx : Chillx() {
+    override val name = "Playerx"
+    override val mainUrl = "https://playerx.stream"
+}
+
+class Moviesapi : Chillx() {
+    override val name = "Moviesapi"
+    override val mainUrl = "https://w1.moviesapi.club"
+}
+
+class AnimesagaStream : Chillx() {
+    override val name = "Animesaga"
+    override val mainUrl = "https://stream.animesaga.in"
+}
+
+class Anplay : Chillx() {
+    override val name = "Anplay"
+    override val mainUrl = "https://stream.anplay.in"
+}
+
+class Kinogeru : Chillx() {
+    override val name = "Kinoger"
+    override val mainUrl = "https://kinoger.ru"
+}
+
+class Vidxstream : Chillx() {
+    override val name = "Vidxstream"
+    override val mainUrl = "https://vidxstream.xyz"
+}
+
+class Boltx : Chillx() {
+    override val name = "Boltx"
+    override val mainUrl = "https://boltx.stream"
+}
+
+class Vectorx : Chillx() {
+    override val name = "Vectorx"
+    override val mainUrl = "https://vectorx.top"
+}
+
+class Boosterx : Chillx() {
+    override val name = "Boosterx"
+    override val mainUrl = "https://boosterx.stream"
 }
 
 open class Chillx : ExtractorApi() {
     override val name = "Chillx"
     override val mainUrl = "https://chillx.top"
     override val requiresReferer = true
-
-    companion object {
-        private val keySource = "https://rowdy-avocado.github.io/multi-keys/"
-
-        private var key: String? = null
-
-        private suspend fun fetchKey(): String {
-            return key
-                    ?: run {
-                        val res =
-                                app.get(keySource).parsedSafe<KeysData>()
-                                        ?: throw ErrorLoadingException("Unable to get keys")
-                        key = res.keys.get(0)
-                        res.keys.get(0)
-                    }
-        }
-
-        private data class KeysData(@JsonProperty("chillx") val keys: List<String>)
-    }
+	private var key: String? = null
 
     @Suppress("NAME_SHADOWING")
-    override suspend fun getUrl(
+	override suspend fun getUrl(
         url: String,
         referer: String?,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
-    ) {
-        val master = Regex("""JScript[\w+]?\s*=\s*'([^']+)""").find(
+    ) {		
+		val master = Regex("\\s*=\\s*'([^']+)").find(
             app.get(
                 url,
-                referer = url,
+                referer = referer ?: "",
+                headers = mapOf(
+                    "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Accept-Language" to "en-US,en;q=0.5",
+                )
             ).text
         )?.groupValues?.get(1)
-        val key = fetchKey()
-        val decrypt = cryptoAESHandler(master ?: "", key.toByteArray(), false)?.replace("\\", "") ?: throw ErrorLoadingException("failed to decrypt")
+		
+		val key = fetchKey() ?: throw ErrorLoadingException("Unable to get key")
+		
+        val decrypt = cryptoAESHandler(master ?: return, key.toByteArray(), false)
+            ?.replace("\\", "")
+            ?: throw ErrorLoadingException("failed to decrypt")
+
         val source = Regex(""""?file"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
+        val name = url.getHost()
+
         val subtitles = Regex("""subtitle"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
-        val subtitlePattern = """\[(.*?)](https?://[^\s,]+)""".toRegex()
+        val subtitlePattern = """\[(.*?)\](https?://[^\s,]+)""".toRegex()
         val matches = subtitlePattern.findAll(subtitles ?: "")
         val languageUrlPairs = matches.map { matchResult ->
             val (language, url) = matchResult.destructured
@@ -79,28 +122,51 @@ open class Chillx : ExtractorApi() {
                 )
             )
         }
-        // required
-        val headers = mapOf(
-            "Accept" to "*/*",
-            "Connection" to "keep-alive",
-            "Sec-Fetch-Dest" to "empty",
-            "Sec-Fetch-Mode" to "cors",
-            "Sec-Fetch-Site" to "cross-site",
-            "Origin" to mainUrl,
+		
+        val header =
+            mapOf(
+                "accept" to "*/*",
+                "accept-language" to "en-US,en;q=0.5",
+                "Origin" to mainUrl,
+                "Accept-Encoding" to "gzip, deflate, br",
+                "Connection" to "keep-alive",
+                "Sec-Fetch-Dest" to "empty",
+                "Sec-Fetch-Mode" to "cors",
+                "Sec-Fetch-Site" to "cross-site",
+                "user-agent" to USER_AGENT,
+            )
+
+		callback.invoke(
+            ExtractorLink(
+                name,
+                name,
+                url = source ?: return,
+                referer = "$mainUrl/",
+                quality = Qualities.Unknown.value,
+				INFER_TYPE,
+                headers = header,
+            )
         )
+    }	
 
-        M3u8Helper.generateM3u8(
-            name,
-            source ?: return,
-            "$mainUrl/",
-            headers = headers
-        ).forEach(callback)
+    private suspend fun fetchKey(): String? {
+        return app.get("https://raw.githubusercontent.com/Rowdy-Avocado/multi-keys/keys/index.html")
+            .parsedSafe<Keys>()?.key?.get(0)?.also { key = it }
     }
-
-    private fun decodeUnicodeEscape(input: String): String {
+	
+	private fun decodeUnicodeEscape(input: String): String {
         val regex = Regex("u([0-9a-fA-F]{4})")
         return regex.replace(input) {
             it.groupValues[1].toInt(16).toChar().toString()
         }
     }
+	
+	private fun String.getHost(): String {
+		return fixTitle(URI(this).host.substringBeforeLast(".").substringAfterLast("."))
+	}
+	
+	data class Keys(
+        @JsonProperty("chillx") val key: List<String>
+    )
+    
 }
