2a8bc18b0fe685079725ab726b339fc636ee7ecb
b4byhuey <60543438+b4byhuey@users.noreply.github.com>
2024-08-24 01:45:08 +0800

    Update VidhideExtractor.kt

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/VidhideExtractor.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/VidhideExtractor.kt
@@ -1,34 +1,93 @@
 package com.lagradost.cloudstream3.extractors
 
+import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.network.WebViewResolver
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
 
-open class VidhideExtractor : ExtractorApi() {
-    override var name = "VidHide"
-    override var mainUrl = "https://vidhide.com"
-    override val requiresReferer = false
+class VidHidePro1 : VidHidePro() {
+    override var mainUrl = "https://filelions.live"
+}
 
-    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
-        val response = app.get(
-            url, referer = referer ?: "$mainUrl/", interceptor = WebViewResolver(
-                Regex("""master\.m3u8""")
-            )
+class VidHidePro2 : VidHidePro() {
+    override var mainUrl = "https://filelions.online"
+}
+
+class VidHidePro3 : VidHidePro() {
+    override var mainUrl = "https://filelions.to"
+}
+
+class VidHidePro4 : VidHidePro() {
+    override val mainUrl = "https://kinoger.be"
+}
+
+class VidHidePro5: VidHidePro() {
+    override val mainUrl = "https://vidhidevip.com"
+}
+
+class VidHidePro6 : VidHidePro() {
+    override val mainUrl = "https://vidhidepre.com"
+}
+
+class Lulustream1 : VidHidePro() {
+    override val name = "Lulustream"
+    override val mainUrl = "https://lulustream.com"
+}
+
+class Lulustream2 : VidHidePro() {
+    override val name = "Lulustream"
+    override val mainUrl = "https://luluvdo.com"
+}
+
+class Lulustream3 : VidHidePro() {
+    override val name = "Lulustream"
+    override val mainUrl = "https://kinoger.pw"
+}
+
+open class VidHidePro : ExtractorApi() {
+    override val name = "VidHidePro"
+    override val mainUrl = "https://vidhidepro.com"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val headers = mapOf(
+            "Accept" to "*/*",
+            "Connection" to "keep-alive",
+            "Sec-Fetch-Dest" to "empty",
+            "Sec-Fetch-Mode" to "cors",
+            "Sec-Fetch-Site" to "cross-site",
+            "Origin" to "$mainUrl/",
+	        "User-Agent" to USER_AGENT,
         )
-        val sources = mutableListOf<ExtractorLink>()
-        if (response.url.contains("m3u8"))
-            sources.add(
-                ExtractorLink(
-                    source = name,
-                    name = name,
-                    url = response.url,
-                    referer = referer ?: "$mainUrl/",
-                    quality = Qualities.Unknown.value,
-                    isM3u8 = true
-                )
-            )
-        return sources
+        
+        val response = app.get(getEmbedUrl(url), referer = referer)
+        val script = if (!getPacked(response.text).isNullOrEmpty()) {
+            getAndUnpack(response.text)
+        } else {
+            response.document.selectFirst("script:containsData(sources:)")?.data()
+        }
+        val m3u8 =
+            Regex("file:\\s*\"(.*?m3u8.*?)\"").find(script ?: return)?.groupValues?.getOrNull(1)
+        generateM3u8(
+            name,
+            m3u8 ?: return,
+            mainUrl,
+	        headers = headers
+        ).forEach(callback)
     }
+
+    private fun getEmbedUrl(url: String): String {
+		return when {
+			url.contains("/d/") -> url.replace("/d/", "/v/")
+			url.contains("/download/") -> url.replace("/download/", "/v/")
+			url.contains("/file/") -> url.replace("/file/", "/v/")
+			else -> url.replace("/f/", "/v/")
+		}
+	}
+
 }
