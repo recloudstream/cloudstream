3af3d9724fe6f9593beb7c93747d188a2d208a48
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2024-09-07 23:24:23 +0200

    fix missing android internal classes

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -2,7 +2,26 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import java.net.URL
 
 plugins {
+    id("com.android.library")
+    id("kotlin-android")
     id("org.jetbrains.dokka")
+}
+
+android {
+    compileSdk = 34
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+    }
+
+    namespace = "com.lagradost.api"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 tasks.withType<DokkaTask>().configureEach {
@@ -12,6 +31,7 @@ tasks.withType<DokkaTask>().configureEach {
             listOf("androidMain", "commonMain", "jvmMain").forEach { srcName ->
                 sourceRoot("../library/src/$srcName/kotlin")
             }
+            classpath.from(android.bootClasspath)
             sourceRoot(file("../app/src/main/java"))
             sourceLink {
                 localDirectory = file("..")
