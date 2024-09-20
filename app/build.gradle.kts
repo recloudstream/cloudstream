4952818ef2e4e66345ee1571ec7e043628878336
KingLucius <a.aelfattah5@gmail.com>
2024-09-20 11:57:19 +0300

    Dependencies Version Catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,84 +152,87 @@ android {
 
 dependencies {
     // Testing
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.json:json:20240303")
-    androidTestImplementation("androidx.test:core")
-    implementation("androidx.test.ext:junit-ktx:1.2.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    testImplementation(libs.junit)
+    testImplementation(libs.json)
+    androidTestImplementation(libs.core)
+    implementation(libs.junit.ktx)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
     // Android Core & Lifecycle
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.navigation.ui.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment.ktx)
 
     // Design & UI
-    implementation("jp.wasabeef:glide-transformations:4.3.0")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation(libs.glide.transformations)
+    implementation(libs.preference.ktx)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.swiperefreshlayout)
 
     // Glide Module
-    ksp("com.github.bumptech.glide:ksp:4.16.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.github.bumptech.glide:okhttp3-integration:4.16.0")
+    ksp(libs.ksp)
+    implementation(libs.glide)
+    implementation(libs.okhttp3.integration)
 
     // For KSP -> Official Annotation Processors are Not Yet Supported for KSP
-    ksp("dev.zacsweers.autoservice:auto-service-ksp:1.2.0")
-    implementation("com.google.guava:guava:33.2.1-android")
-    implementation("dev.zacsweers.autoservice:auto-service-ksp:1.2.0")
+    ksp(libs.auto.service.ksp)
+    implementation(libs.guava)
+    implementation(libs.auto.service.ksp)
 
     // Media 3 (ExoPlayer)
-    implementation("androidx.media3:media3-ui:1.4.0")
-    implementation("androidx.media3:media3-cast:1.4.0")
-    implementation("androidx.media3:media3-common:1.4.0")
-    implementation("androidx.media3:media3-session:1.4.0")
-    implementation("androidx.media3:media3-exoplayer:1.4.0")
-    implementation("com.google.android.mediahome:video:1.0.0")
-    implementation("androidx.media3:media3-exoplayer-hls:1.4.0")
-    implementation("androidx.media3:media3-exoplayer-dash:1.4.0")
-    implementation("androidx.media3:media3-datasource-okhttp:1.4.0")
+    implementation(libs.media3.ui)
+    implementation(libs.media3.cast)
+    implementation(libs.media3.common)
+    implementation(libs.media3.session)
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.exoplayer.hls)
+    implementation(libs.media3.exoplayer.dash)
+    implementation(libs.media3.datasource.okhttp)
+    implementation(libs.video)
 
     // PlayBack
-    implementation("com.jaredrummler:colorpicker:1.1.0") // Subtitle Color Picker
-    implementation("com.github.recloudstream:media-ffmpeg:1.1.0") // Custom FF-MPEG Lib for Audio Codecs
-    implementation("com.github.teamnewpipe:NewPipeExtractor:v0.24.2") /* For Trailers
+    implementation(libs.colorpicker) // Subtitle Color Picker
+    implementation(libs.media.ffmpeg) // Custom FF-MPEG Lib for Audio Codecs
+    implementation(libs.newpipeextractor) /* For Trailers
     ^ Update to Latest Commits if Trailers Misbehave, github.com/TeamNewPipe/NewPipeExtractor/commits/dev */
-    implementation("com.github.albfernandez:juniversalchardet:2.5.0") // Subtitle Decoding
+    implementation(libs.juniversalchardet) // Subtitle Decoding
 
     // Crash Reports (AcraApplication.kt)
-    implementation("ch.acra:acra-core:5.11.3")
-    implementation("ch.acra:acra-toast:5.11.3")
+    implementation(libs.acra.core)
+    implementation(libs.acra.toast)
 
     // UI Stuff
-    implementation("com.facebook.shimmer:shimmer:0.5.0") // Shimmering Effect (Loading Skeleton)
-    implementation("androidx.palette:palette-ktx:1.0.0") // Palette For Images -> Colors
-    implementation("androidx.tvprovider:tvprovider:1.0.0")
-    implementation("com.github.discord:OverlappingPanels:0.1.5") // Gestures
-    implementation("androidx.biometric:biometric:1.2.0-alpha05") // Fingerprint Authentication
-    implementation("com.github.rubensousa:previewseekbar-media3:1.1.1.0") // SeekBar Preview
-    implementation("io.github.g0dkar:qrcode-kotlin:4.2.0") // QR code for PIN Auth on TV
+    implementation(libs.shimmer) // Shimmering Effect (Loading Skeleton)
+    implementation(libs.palette.ktx) // Palette For Images -> Colors
+    implementation(libs.tvprovider)
+    implementation(libs.overlappingpanels) // Gestures
+    implementation(libs.biometric) // Fingerprint Authentication
+    implementation(libs.previewseekbar.media3) // SeekBar Preview
+    implementation(libs.qrcode.kotlin) // QR code for PIN Auth on TV
 
     // Extensions & Other Libs
-    implementation("org.mozilla:rhino:1.7.15") // run JavaScript
-    implementation("me.xdrop:fuzzywuzzy:1.4.0") // Library/Ext Searching with Levenshtein Distance
-    implementation("com.github.LagradOst:SafeFile:0.0.6") // To Prevent the URI File Fu*kery
-    implementation("org.conscrypt:conscrypt-android:2.5.2") // To Fix SSL Fu*kery on Android 9
-    implementation("com.uwetrottmann.tmdb2:tmdb-java:2.11.0") // TMDB API v3 Wrapper Made with RetroFit
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_nio:2.0.4") //nio flavor needed for NewPipeExtractor
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") /* JSON Parser
-    ^ Don't Bump Jackson above 2.13.1 , Crashes on Android TV's and FireSticks that have Min API
-    Level 25 or Less. */
+    implementation(libs.rhino) // run JavaScript
+    implementation(libs.fuzzywuzzy) // Library/Ext Searching with Levenshtein Distance
+    implementation(libs.safefile) // To Prevent the URI File Fu*kery
+    implementation(libs.conscrypt.android) // To Fix SSL Fu*kery on Android 9
+    implementation(libs.tmdb.java) // TMDB API v3 Wrapper Made with RetroFit
+    coreLibraryDesugaring(libs.desugar.jdk.libs.nio) //nio flavor needed for NewPipeExtractor
+    implementation(libs.jackson.module.kotlin) {
+        version {
+            strictly("2.13.1")
+        }
+        because("Don't Bump Jackson above 2.13.1 , Crashes on Android TV's and FireSticks that have Min API Level 25 or Less.")
+    } //JSON Parser
 
     // Downloading & Networking
-    implementation("androidx.work:work-runtime:2.9.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
-    implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
+    implementation(libs.work.runtime)
+    implementation(libs.work.runtime.ktx)
+    implementation(libs.nicehttp) // HTTP Lib
 
     implementation(project(":library") {
         // There does not seem to be a good way of getting the android flavor.
