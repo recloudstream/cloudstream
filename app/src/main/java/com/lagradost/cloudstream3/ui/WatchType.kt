2a08a64b60a093ed26a4a3a7bdc541ef5a2bf56e
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-14 12:18:21 -0700

    Fix order, cleanup, and add docs to WatchType

--- app/src/main/java/com/lagradost/cloudstream3/ui/WatchType.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/WatchType.kt
@@ -3,42 +3,67 @@ package com.lagradost.cloudstream3.ui
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.lagradost.cloudstream3.R
+import com.lagradost.cloudstream3.syncproviders.SyncAPI
 
+/**
+ * Enum class representing watch states for library.
+ *
+ * @property internalId The unique ID used internally to identify the watch type and to store data. This value must never be changed.
+ * @property stringRes The string resource ID representing the watch type name.
+ * @property iconRes The drawable resource ID representing the watch type icon.
+ */
 enum class WatchType(val internalId: Int, @StringRes val stringRes: Int, @DrawableRes val iconRes: Int) {
-    NONE(-1, R.string.type_none, R.drawable.ic_baseline_add_24),
-    WATCHING(0, R.string.type_watching, R.drawable.ic_baseline_bookmark_24),
-    COMPLETED(1, R.string.type_completed, R.drawable.ic_baseline_bookmark_24),
-    ONHOLD(2, R.string.type_on_hold, R.drawable.ic_baseline_bookmark_24),
-    DROPPED(3, R.string.type_dropped, R.drawable.ic_baseline_bookmark_24),
-    PLANTOWATCH(4, R.string.type_plan_to_watch, R.drawable.ic_baseline_bookmark_24),
-    NOTINTERESTED(5, R.string.type_not_interested, R.drawable.ic_baseline_bookmark_24);
+    WATCHING(internalId = 0, R.string.type_watching, R.drawable.ic_baseline_bookmark_24),
+    COMPLETED(internalId = 1, R.string.type_completed, R.drawable.ic_baseline_bookmark_24),
+    ONHOLD(internalId = 2, R.string.type_on_hold, R.drawable.ic_baseline_bookmark_24),
+    DROPPED(internalId = 3, R.string.type_dropped, R.drawable.ic_baseline_bookmark_24),
+    PLANTOWATCH(internalId = 4, R.string.type_plan_to_watch, R.drawable.ic_baseline_bookmark_24),
+
+    // Any types with negative internal IDs, or those for which the order is important,
+    // should be placed at the bottom. This ensures that we don't need to worry about
+    // their internal IDs when adding new types in the future.
+    NOTINTERESTED(internalId = -2, R.string.type_not_interested, R.drawable.ic_baseline_bookmark_24),
+    NONE(internalId = -1, R.string.type_none, R.drawable.ic_baseline_add_24);
 
     companion object {
+        /**
+         * Finds a [WatchType] corresponding to the given [internalId].
+         *
+         * @param id The internal ID to search for.
+         * @return The corresponding [WatchType], or [NONE] if no match is found.
+         */
         fun fromInternalId(id: Int?) = entries.find { value -> value.internalId == id } ?: NONE
     }
 }
 
+/**
+ * Enum class representing various watch states for when using a remote library in [SyncAPI].
+ *
+ * @property internalId The unique ID used internally to identify the watch type and to store data locally. This value must never be changed.
+ * @property stringRes The string resource ID representing the watch type name.
+ * @property iconRes The drawable resource ID representing the watch type icon.
+ */
 enum class SyncWatchType(val internalId: Int, @StringRes val stringRes: Int, @DrawableRes val iconRes: Int) {
-    /**
-     * -1 -> None
-     * 0 -> Watching
-     * 1 -> Completed
-     * 2 -> OnHold
-     * 3 -> Dropped
-     * 4 -> PlanToWatch
-     * 5 -> ReWatching
-     * 6 -> NotInterested
-     */
-    NONE(-1, R.string.type_none, R.drawable.ic_baseline_add_24),
-    WATCHING(0, R.string.type_watching, R.drawable.ic_baseline_bookmark_24),
-    COMPLETED(1, R.string.type_completed, R.drawable.ic_baseline_bookmark_24),
-    ONHOLD(2, R.string.type_on_hold, R.drawable.ic_baseline_bookmark_24),
-    DROPPED(3, R.string.type_dropped, R.drawable.ic_baseline_bookmark_24),
-    PLANTOWATCH(4, R.string.type_plan_to_watch, R.drawable.ic_baseline_bookmark_24),
-    REWATCHING(5, R.string.type_re_watching, R.drawable.ic_baseline_bookmark_24),
-    NOTINTERESTED(6, R.string.type_not_interested, R.drawable.ic_baseline_bookmark_24);
+    NONE(internalId = -1, R.string.type_none, R.drawable.ic_baseline_add_24),
+    WATCHING(internalId = 0, R.string.type_watching, R.drawable.ic_baseline_bookmark_24),
+    COMPLETED(internalId = 1, R.string.type_completed, R.drawable.ic_baseline_bookmark_24),
+    ONHOLD(internalId = 2, R.string.type_on_hold, R.drawable.ic_baseline_bookmark_24),
+    DROPPED(internalId = 3, R.string.type_dropped, R.drawable.ic_baseline_bookmark_24),
+    PLANTOWATCH(internalId = 4, R.string.type_plan_to_watch, R.drawable.ic_baseline_bookmark_24),
+    REWATCHING(internalId = 5, R.string.type_re_watching, R.drawable.ic_baseline_bookmark_24),
+
+    // Any types with negative internal IDs, or those for which the order is important, 
+    // should be placed at the bottom, except for "NONE", which has a negative ID but is 
+    // placed at the top for UI reasons.
+    NOTINTERESTED(internalId = -2, R.string.type_not_interested, R.drawable.ic_baseline_bookmark_24);
 
     companion object {
+        /**
+         * Finds a [SyncWatchType] corresponding to the given [internalId].
+         *
+         * @param id The internal ID to search for.
+         * @return The corresponding [SyncWatchType], or [NONE] if no match is found.
+         */
         fun fromInternalId(id: Int?) = entries.find { value -> value.internalId == id } ?: NONE
     }
 }
