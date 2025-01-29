2c6656d3c04e9a83fe02a34f0c98a0e718f60805
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-29 12:35:54 -0700

    Rename torrentserver package ref to match new package (#1519)
    * Rename torrentserver package ref to match new package
    
    * Order

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -221,7 +221,7 @@ dependencies {
     } // JSON Parser
 
     // Torrent Support
-    implementation(libs.torrentserver.aniyomi)
+    implementation(libs.torrentserver)
 
     // Downloading & Networking
     implementation(libs.work.runtime)
