75bcf967d14dc714417966957ff475577bd4dd70
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-02-05 14:40:38 -0700

    Move metaproviders to library
    So that providers can extend them when using the library dependency.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
             implementation(libs.fuzzywuzzy) // Match Extractors
             implementation(libs.rhino) // Run JavaScript
             implementation(libs.newpipeextractor)
+            implementation(libs.tmdb.java) // TMDB API v3 Wrapper Made with RetroFit
         }
     }
 }
