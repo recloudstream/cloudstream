71b5c694261e30d1a704b23e0ee4bfd4f993a1dc
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-02-11 06:42:48 -0700

    Replace use of gson in PluginManager (#1544)

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/AppUtils.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/AppUtils.kt
@@ -2,6 +2,7 @@ package com.lagradost.cloudstream3.utils
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.lagradost.cloudstream3.mapper
+import java.io.Reader
 
 object AppUtils {
     /** Any object as json string */
@@ -12,6 +13,10 @@ object AppUtils {
 
     inline fun <reified T> parseJson(value: String): T {
         return mapper.readValue(value)
+    }
+
+    inline fun <reified T> parseJson(reader: Reader, valueType: Class<T>): T {
+        return mapper.readValue(reader, valueType)
     }
 
     inline fun <reified T> tryParseJson(value: String?): T? {
