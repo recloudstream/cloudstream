e3b15a1531ebd7ec677af9c1beeb757041e32026
KingLucius <a.aelfattah5@gmail.com>
2025-08-08 10:19:17 +0300

    Update SettingsAccount.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsAccount.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsAccount.kt
@@ -40,6 +40,7 @@ import com.lagradost.cloudstream3.syncproviders.SubtitleRepo
 import com.lagradost.cloudstream3.syncproviders.SyncRepo
 import com.lagradost.cloudstream3.ui.settings.Globals.EMULATOR
 import com.lagradost.cloudstream3.ui.settings.Globals.TV
+import com.lagradost.cloudstream3.ui.settings.Globals.PHONE
 import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.getPref
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.hideOn
