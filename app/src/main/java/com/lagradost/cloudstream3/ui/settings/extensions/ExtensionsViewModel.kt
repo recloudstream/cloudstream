fbfa207abebc2f54b00da350ec989929ec84027d
rockhero1234 <techartistdilip@gmail.com>
2025-08-17 17:55:37 +0530

    fix:retain old RepositoryData constructor

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/extensions/ExtensionsViewModel.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/extensions/ExtensionsViewModel.kt
@@ -20,7 +20,9 @@ data class RepositoryData(
     @JsonProperty("iconUrl") val iconUrl: String?,
     @JsonProperty("name") val name: String,
     @JsonProperty("url") val url: String
-)
+){
+    constructor(name: String,url: String):this(null,name,url)
+}
 
 const val REPOSITORIES_KEY = "REPOSITORIES_KEY"
 
