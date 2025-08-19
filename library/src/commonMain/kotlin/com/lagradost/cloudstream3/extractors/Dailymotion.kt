6a6704c6945ef010b468649991fbc0d61c346676
Phisher98 <mandiktest2@gmail.com>
2025-08-19 14:45:50 +0530

    Dailymotion

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/Dailymotion.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/Dailymotion.kt
@@ -1,6 +1,5 @@
-package recloudstream
+package com.lagradost.cloudstream3.extractors
 
-import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
