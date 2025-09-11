50636cea6086046394e9089579b9a0068aed31b8
Jimly Asshiddiqy <jimly.asshiddiqy@accenture.com>
2025-09-11 09:15:53 +0700

    chore(Gradle): Improve Gradle Configuration
    - Use version catalog for applying gradle plugin
    - Removing unused entry in version catalog
    - Removing docs module and instead setup dokka at the root project
    - Bump JVM target from 1.8 to 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,33 @@
 rootProject.name = "CloudStream"
 
-include(":app")
-include(":library")
-include(":docs")
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        mavenLocal()
+        maven("https://jitpack.io")
+    }
+}
+
+include(":app", ":library")
