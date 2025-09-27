658dd05f986b7ae7e8f60aa3b39fe2724020891f
rockhero1234 <techartistdilip@gmail.com>
2025-09-27 18:35:57 +0530

    cleanup

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/FullScreenPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/FullScreenPlayer.kt
@@ -64,7 +64,6 @@ import com.lagradost.cloudstream3.ui.settings.Globals.EMULATOR
 import com.lagradost.cloudstream3.ui.settings.Globals.PHONE
 import com.lagradost.cloudstream3.ui.settings.Globals.TV
 import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
-import com.lagradost.cloudstream3.utils.AppContextUtils.isRtl
 import com.lagradost.cloudstream3.utils.AppContextUtils.isUsingMobileData
 import com.lagradost.cloudstream3.utils.DataStoreHelper
 import com.lagradost.cloudstream3.utils.SingleSelectionHelper.showDialog
