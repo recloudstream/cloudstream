aa7a0f053a1ac8c00331ac3edb535f7963a1bbb0
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-13 15:32:59 -0700

    Fix setting `failOnNoDiscoveredTests = false`

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
 
-// https://docs.gradle.org/current/userguide/upgrading_major_version_9.html#test_task_fails_when_no_tests_are_discovered
-tasks.withType<AbstractTestTask>().configureEach {
-    failOnNoDiscoveredTests = false
+allprojects {
+    // https://docs.gradle.org/current/userguide/upgrading_major_version_9.html#test_task_fails_when_no_tests_are_discovered
+    tasks.withType<AbstractTestTask>().configureEach {
+        failOnNoDiscoveredTests = false
+    }
 }
