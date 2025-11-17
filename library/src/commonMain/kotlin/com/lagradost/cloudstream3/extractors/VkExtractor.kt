aee677bb708446bf9bbee66dce0f13f431fc0bc8
Kraptor123 <kraptor123@gmail.com>
2025-11-17 05:09:02 +0300

    VkExtractor

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/VkExtractor.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/VkExtractor.kt
@@ -12,7 +12,7 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 open class VkExtractor : ExtractorApi() {
     override val name = "Vk"
     override val mainUrl = "https://vkvideo.ru"
-    override val requiresReferer = false
+    override val requiresReferer = true
     override suspend fun getUrl(
         url: String,
         referer: String?,
@@ -35,57 +35,60 @@ open class VkExtractor : ExtractorApi() {
             "Cache-Control" to "no-cache"
         )
         val cookie = app.get(url, headers = headers, allowRedirects = false).cookies
+        val response = app.get(url, headers = headers, allowRedirects = false, cookies = cookie).text
+        val listUrl = listOf("url", "dash_sep", "hls")
 
-        val response =
-            app.get(url, headers = headers, allowRedirects = false, cookies = cookie).text
-
-        val listUrl = listOf("url144", "url240", "url360", "url480", "url720", "dash_sep", "hls")
-
-        listUrl.forEach { link ->
-            val regex =
-                Regex(pattern = "\"$link\":\"([^\"]*)\"", options = setOf(RegexOption.IGNORE_CASE))
-
-            val video = regex.find(response)?.groupValues[1]?.replace("\\", "").toString()
-
-            val type = if (link.contains("hls")) {
-                "HLS"
-            } else if (link.contains("dash")) {
-                "Dash"
+        listUrl.forEach { linkType ->
+            if (linkType == "url") {
+                val regex = Regex(pattern = "\"url([0-9]+)\":\"([^\"]*)\"", options = setOf(RegexOption.IGNORE_CASE))
+                regex.findAll(response).forEach { url ->
+                    val video = url.groupValues[2].replace("\\", "")
+                    val quality = url.groupValues[1].replace("\\", "")
+                    callback.invoke(
+                        newExtractorLink(
+                            this.name,
+                            this.name,
+                            video,
+                            ExtractorLinkType.VIDEO
+                        ) {
+                            this.referer = "${mainUrl}/"
+                            this.headers = mapOf(
+                                "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",
+                                "Accept" to "*/*",
+                                "Accept-Language" to "en-US,en;q=0.5",
+                                "Referer" to "${mainUrl}/",
+                            )
+                            this.quality = getQualityFromName(quality)
+                        })
+                }
             } else {
-                ""
+                val regex = Regex(pattern = "\"$linkType\":\"([^\"]*)\"", options = setOf(RegexOption.IGNORE_CASE))
+                val video = regex.find(response)?.groupValues?.getOrNull(1)?.replace("\\", "") ?: return@forEach
+                val type = when {
+                    linkType.contains("hls") -> "HLS"
+                    linkType.contains("dash") -> "Dash"
+                    else -> ""
+                }
+                callback.invoke(
+                    newExtractorLink(
+                        "${this.name} $type",
+                        "${this.name} $type",
+                        video,
+                        when {
+                            linkType.contains("dash") -> ExtractorLinkType.DASH
+                            linkType.contains("hls") -> ExtractorLinkType.M3U8
+                            else -> ExtractorLinkType.VIDEO
+                        }
+                    ) {
+                        this.referer = "${mainUrl}/"
+                        this.headers = mapOf(
+                            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",
+                            "Accept" to "*/*",
+                            "Accept-Language" to "en-US,en;q=0.5",
+                            "Referer" to "${mainUrl}/",
+                        )
+                    })
             }
-
-            callback.invoke(
-                newExtractorLink(
-                    "${this.name} $type",
-                    "${this.name} $type",
-                    video,
-                    if (link.contains("dash")) {
-                        ExtractorLinkType.DASH
-                    } else if (link.contains("hls")) {
-                        ExtractorLinkType.M3U8
-                    } else {
-                        ExtractorLinkType.VIDEO
-                    }
-
-                ) {
-                    this.referer = "${mainUrl}/"
-                    this.headers = mapOf(
-                        "Host" to "vkvd1.okcdn.ru",
-                        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",
-                        "Accept" to "*/*",
-                        "Accept-Language" to "en-US,en;q=0.5",
-                        "Referer" to "${mainUrl}/",
-                        "Sec-GPC" to "1",
-                        "Connection" to "keep-alive",
-                        "Sec-Fetch-Dest" to "empty",
-                        "Sec-Fetch-Mode" to "cors",
-                        "Sec-Fetch-Site" to "cross-site",
-                        "Pragma" to "no-cache",
-                        "Cache-Control" to "no-cache"
-                    )
-                    this.quality = getQualityFromName(link.substringAfter("url"))
-                })
         }
     }
 }
