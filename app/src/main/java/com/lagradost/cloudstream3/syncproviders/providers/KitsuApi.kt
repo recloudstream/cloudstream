8fe60ddece40a3ffee27e1b251ba4224e76da75f
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-01-08 20:00:20 +0100

    Kitsu tab now works

--- app/src/main/java/com/lagradost/cloudstream3/syncproviders/providers/KitsuApi.kt
+++ app/src/main/java/com/lagradost/cloudstream3/syncproviders/providers/KitsuApi.kt
@@ -2,8 +2,12 @@ package com.lagradost.cloudstream3.syncproviders.providers
 
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.CloudStreamApp.Companion.getKey
+import com.lagradost.cloudstream3.CloudStreamApp.Companion.setKey
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.R
+import com.lagradost.cloudstream3.Score
+import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.mvvm.logError
 import com.lagradost.cloudstream3.syncproviders.AuthData
 import com.lagradost.cloudstream3.syncproviders.AuthLoginRequirement
@@ -12,7 +16,16 @@ import com.lagradost.cloudstream3.syncproviders.AuthToken
 import com.lagradost.cloudstream3.syncproviders.AuthUser
 import com.lagradost.cloudstream3.syncproviders.SyncAPI
 import com.lagradost.cloudstream3.syncproviders.SyncIdName
+import com.lagradost.cloudstream3.syncproviders.providers.MALApi.Companion.MalStatusType
 import com.lagradost.cloudstream3.ui.SyncWatchType
+import com.lagradost.cloudstream3.ui.library.ListSorting
+import com.lagradost.cloudstream3.utils.DataStore.toKotlinObject
+import com.lagradost.cloudstream3.utils.txt
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Locale
 
 class KitsuApi: SyncAPI() {
     override var name = "Kitsu"
@@ -25,6 +38,7 @@ class KitsuApi: SyncAPI() {
     override val icon = R.drawable.kitsu_icon
     override val syncIdName = SyncIdName.Kitsu
     override val createAccountUrl = mainUrl
+    val KITSU_CACHED_LIST: String = "kitsu_cached_list"
 
     override val supportedWatchTypes = setOf(
         SyncWatchType.WATCHING,
@@ -96,7 +110,7 @@ class KitsuApi: SyncAPI() {
         )
     }
 
-    override suspend fun search(auth: AuthData?, query: String): List<SyncAPI.SyncSearchResult>? {
+    override suspend fun search(auth: AuthData?, query: String): List<SyncSearchResult>? {
         val auth = auth?.token?.accessToken ?: return null
         val url = "$apiUrl/anime?filter[text]=$query&page[limit]=$KITSU_MAX_SEARCH_LIMIT"
         val res = app.get(
@@ -109,10 +123,10 @@ class KitsuApi: SyncAPI() {
 
             val title = attributes.canonicalTitle ?: attributes.titles?.enJp ?: attributes.titles?.jaJp ?: "No title"
 
-            SyncAPI.SyncSearchResult(
+            SyncSearchResult(
                 title,
                 this.name,
-                it.id.toString(),
+                it.id,
                 "$mainUrl/anime/${it.id}/",
                 attributes.posterImage?.large ?: attributes.posterImage?.medium
             )
@@ -122,6 +136,70 @@ class KitsuApi: SyncAPI() {
     override fun urlToId(url: String): String? =
         Regex("""/anime/((.*)/|(.*))""").find(url)!!.groupValues.first()
 
+    override suspend fun library(auth : AuthData?): LibraryMetadata? {
+        val list = getKitsuAnimeListSmart(auth ?: return null)?.groupBy {
+            convertToStatus(it.attributes.status ?: "").stringRes
+        }?.mapValues { group ->
+            group.value.map { it.toLibraryItem(auth.token) }
+        } ?: emptyMap()
+
+        // To fill empty lists when MAL does not return them
+        val baseMap =
+            MalStatusType.entries.filter { it.value >= 0 }.associate {
+                it.stringRes to emptyList<LibraryItem>()
+            }
+
+        return LibraryMetadata(
+            (baseMap + list).map { LibraryList(txt(it.key), it.value) },
+            setOf(
+                ListSorting.AlphabeticalA,
+                ListSorting.AlphabeticalZ,
+                ListSorting.UpdatedNew,
+                ListSorting.UpdatedOld,
+                ListSorting.ReleaseDateNew,
+                ListSorting.ReleaseDateOld,
+                ListSorting.RatingHigh,
+                ListSorting.RatingLow,
+            )
+        )
+    }
+
+    private suspend fun getKitsuAnimeListSmart(auth : AuthData): Array<KitsuNode>? {
+        return if (requireLibraryRefresh) {
+            val list = getKitsuAnimeList(auth.token) ?: return null
+            setKey(KITSU_CACHED_LIST, auth.user.id.toString(), list)
+            list
+        } else {
+            getKey<Array<KitsuNode>>(KITSU_CACHED_LIST, auth.user.id.toString()) as? Array<KitsuNode>
+        }
+    }
+
+    private suspend fun getKitsuAnimeList(token: AuthToken): Array<KitsuNode>? {
+        var offset = 0
+        val fullList = mutableListOf<KitsuNode>()
+        val offsetRegex = Regex("""offset=(\d+)""")
+        val userId = user(token)?.id ?: return null
+        while (true) {
+            val data: KitsuResponse = getKitsuAnimeListSlice(token, userId, offset) ?: break
+            fullList.addAll(data.data)
+            offset =
+                data.links?.next?.let { offsetRegex.find(it)?.groupValues?.get(1)?.toInt() }
+                    ?: break
+        }
+        return fullList.toTypedArray()
+    }
+
+    private suspend fun getKitsuAnimeListSlice(token: AuthToken, userId: Int, offset: Int = 0): KitsuResponse? {
+
+        val url =
+            "$apiUrl/library-entries?filter[userId]=$userId&filter[kind]=anime&page[limit]=100&page[offset]=$offset"
+        val res = app.get(
+            url, headers = mapOf(
+                "Authorization" to "Bearer ${token.accessToken}",
+            ), cacheTime = 0
+        ).parsed<KitsuResponse>()
+        return res
+    }
 //    override suspend fun updateStatus(
 //        auth: AuthData?,
 //        id: String,
@@ -144,37 +222,100 @@ class KitsuApi: SyncAPI() {
         @JsonProperty("refresh_token") val refreshToken: String,
     )
 
-//    data class MalDatum(
-//        @JsonProperty("node") val node: MalNode,
-//        @JsonProperty("list_status") val listStatus: MalStatus,
-//    )
-
     data class KitsuNode(
         @JsonProperty("id") val id: String,
-        @JsonProperty("attributes") val attributes: KitsuNodeAttributes
-        /*
-        also, but not used
-        main_picture ->
-            public string medium;
-			public string large;
-         */
-    )
+        @JsonProperty("attributes") val attributes: KitsuNodeAttributes,
+        /* User list anime node */
+        @JsonProperty("relationships") val relationships: KitsuRelationships?,
+    ) {
+        suspend fun toLibraryItem(token: AuthToken): LibraryItem {
+
+            val animeItem = getAnimeItem(token)
+
+            val numEpisodes = animeItem?.attributes?.episodeCount
+
+            val startDate = animeItem?.attributes?.startDate
+
+            val posterImage = animeItem?.attributes?.posterImage
+
+            val canonicalTitle = animeItem?.attributes?.canonicalTitle
+            val titles = animeItem?.attributes?.titles
+
+            val animeId = animeItem?.id
+
+            val synopsis: String? = animeItem?.attributes?.synopsis
+
+            return LibraryItem(
+                canonicalTitle ?: titles?.enJp ?: titles?.jaJp.orEmpty(),
+                "https://kitsu.app/anime/${animeId}/",
+                this.id,
+                this.attributes.progress,
+                numEpisodes,
+                Score.from5(this.attributes.rating),
+                parseDateLong(this.attributes.updatedAt),
+                "Kitsu",
+                TvType.Anime,
+                posterImage?.large ?: posterImage?.medium,
+                null,
+                null,
+                plot = synopsis,
+                releaseDate = if (startDate == null) null else try {
+                    Date.from(
+                        Instant.from(
+                            DateTimeFormatter.ofPattern(if (startDate.length == 4) "yyyy" else if (startDate.length == 7) "yyyy-MM" else "yyyy-MM-dd")
+                                .parse(startDate)
+                        )
+                    )
+                } catch (_: RuntimeException) {
+                    null
+                }
+            )
+        }
+
+        private suspend fun getAnimeItem(token: AuthToken): KitsuNode? {
+
+            val url = this.relationships?.anime?.links?.related ?: return null
+
+            val res = app.get(
+                url, headers = mapOf(
+                    "Authorization" to "Bearer ${token.accessToken}",
+                ), cacheTime = 0).parsed<KitsuResponseUnique>()
+
+            return res.data
+        }
+    }
 
     data class KitsuNodeAttributes(
+        /* General attributes */
         @JsonProperty("titles") val titles: KitsuTitles?,
         @JsonProperty("canonicalTitle") val canonicalTitle: String?,
         @JsonProperty("posterImage") val posterImage: KitsuPosterImage?,
+        @JsonProperty("synopsis") val synopsis: String?,
+        @JsonProperty("startDate") val startDate: String?,
+        @JsonProperty("episodeCount") val episodeCount: Int,
         /* User attributes */
         @JsonProperty("name") val name: String?,
         @JsonProperty("location") val location: String?,
         @JsonProperty("createdAt") val createdAt: String?,
-        @JsonProperty("avatar") val avatar: KitsuUserAvatar?
+        @JsonProperty("avatar") val avatar: KitsuUserAvatar?,
+        /* User list anime attributes */
+        @JsonProperty("progress") val progress: Int?,
+        @JsonProperty("rating") val rating: String?,
+        @JsonProperty("updatedAt") val updatedAt: String?,
+        @JsonProperty("status") val status: String?,
+    )
+
+    data class KitsuRelationships(
+        @JsonProperty("anime") val anime: KitsuRelationshipsAnime?
+    )
+
+    data class KitsuRelationshipsAnime(
+        @JsonProperty("links") val links: KitsuLinks?
     )
 
     data class KitsuPosterImage(
         @JsonProperty("large") val large: String?,
         @JsonProperty("medium") val medium: String?,
-
     )
 
     data class KitsuTitles(
@@ -182,53 +323,50 @@ class KitsuApi: SyncAPI() {
         @JsonProperty("ja_jp") val jaJp: String?
     )
 
-//    data class MalStatus(
-//        @JsonProperty("status") val status: String,
-//        @JsonProperty("score") val score: Int,
-//        @JsonProperty("num_episodes_watched") val numEpisodesWatched: Int,
-//        @JsonProperty("is_rewatching") val isRewatching: Boolean,
-//        @JsonProperty("updated_at") val updatedAt: String,
-//    )
-
     data class KitsuUserAvatar(
         @JsonProperty("original") val original: String?
     )
 
-    data class MalMainPicture(
-        @JsonProperty("large") val large: String?,
-        @JsonProperty("medium") val medium: String?,
-    )
-
-    // Used for getDataAboutId()
-//    data class SmallMalAnime(
-//        @JsonProperty("id") val id: Int,
-//        @JsonProperty("title") val title: String?,
-//        @JsonProperty("num_episodes") val numEpisodes: Int,
-//        @JsonProperty("my_list_status") val myListStatus: MalStatus?,
-//        @JsonProperty("main_picture") val mainPicture: MalMainPicture?,
-//    )
-
-//    data class MalSearchNode(
-//        @JsonProperty("node") val node: Node,
-//    )
-
-    data class KitsuSearchLinks(
+    data class KitsuLinks(
+        /* Pagination */
         @JsonProperty("first") val first: String?,
         @JsonProperty("next") val next: String?,
-        @JsonProperty("last") val last: String?
+        @JsonProperty("last") val last: String?,
+        /* Relationships */
+        @JsonProperty("related") val related: String?
     )
 
     data class KitsuResponse(
-        @JsonProperty("links") val links: KitsuSearchLinks?,
+        @JsonProperty("links") val links: KitsuLinks?,
         @JsonProperty("data") val data: List<KitsuNode>
-        //paging
     )
 
-//    data class MalTitleHolder(
-//        val status: MalStatus,
-//        val id: Int,
-//        val name: String,
-//    )
+    data class KitsuResponseUnique(
+        @JsonProperty("data") val data: KitsuNode
+    )
+
+    companion object {
+        private fun parseDateLong(string: String?): Long? {
+            return try {
+                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault()).parse(
+                    string ?: return null
+                )?.time?.div(1000)
+            } catch (e: Exception) {
+                null
+            }
+        }
+
+        private fun convertToStatus(string: String): MalStatusType {
+            return when (string) {
+                "current" -> MalStatusType.Watching
+                "completed" -> MalStatusType.Completed
+                "on_hold" -> MalStatusType.OnHold
+                "dropped" -> MalStatusType.Dropped
+                "planned" -> MalStatusType.PlanToWatch
+                else -> MalStatusType.None
+            }
+        }
+    }
 }
 
 // modified code from from https://github.com/saikou-app/saikou/blob/main/app/src/main/java/ani/saikou/others/Kitsu.kt
