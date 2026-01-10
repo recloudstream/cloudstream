dfaa25dbd16c2a79455427f1894679d451ebc00c
Phisher98 <mandiktest2@gmail.com>
2026-01-10 10:11:46 +0530

    Add background posters on mobile (vertical)

--- app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeScrollAdapter.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeScrollAdapter.kt
@@ -13,7 +13,6 @@ import com.lagradost.cloudstream3.ui.ViewHolderState
 import com.lagradost.cloudstream3.ui.result.ResultFragment.bindLogo
 import com.lagradost.cloudstream3.ui.settings.Globals.EMULATOR
 import com.lagradost.cloudstream3.ui.settings.Globals.TV
-import com.lagradost.cloudstream3.ui.settings.Globals.isLandscape
 import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
 import com.lagradost.cloudstream3.utils.AppContextUtils.html
 import com.lagradost.cloudstream3.utils.ImageLoader.loadImage
