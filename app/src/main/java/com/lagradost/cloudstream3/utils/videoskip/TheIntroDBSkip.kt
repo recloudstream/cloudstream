aeb371812fa9771727ea021697930baab439c174
firelight <147925818+fire-light42@users.noreply.github.com>
2026-04-08 23:57:51 +0000

    Feat: TheIntroDBSkip + Bugfix

---
+++ app/src/main/java/com/lagradost/cloudstream3/utils/videoskip/TheIntroDBSkip.kt
@@ -0,0 +1,61 @@
+package com.lagradost.cloudstream3.utils.videoskip
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.LoadResponse.Companion.getTMDbId
+import com.lagradost.cloudstream3.LoadResponse.Companion.isMovie
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.ui.result.ResultEpisode
+import com.lagradost.cloudstream3.app
+
+class TheIntroDBSkip : SkipAPI() {
+    override val name = "TheIntroDB"
+    override val supportedTypes = setOf(TvType.TvSeries, TvType.Cartoon, TvType.Anime, TvType.Movie)
+
+    val mainUrl = "https://api.theintrodb.org"
+
+    override suspend fun stamps(
+        data: LoadResponse,
+        episode: ResultEpisode,
+        episodeDurationMs: Long
+    ): List<SkipStamp>? {
+        val tmdbId = data.getTMDbId() ?: return null
+
+        val url = if (data.isMovie()) {
+            "$mainUrl/v2/media?tmdb_id=$tmdbId"
+        } else {
+            "$mainUrl/v2/media?tmdb_id=$tmdbId&season=${episode.season ?: 1}&episode=${episode.episode}"
+        }
+        val root = app.get(url).parsed<Root>()
+        return arrayOf(
+            root.intro to SkipType.Intro,
+            root.credits to SkipType.Credits,
+            root.recap to SkipType.Recap,
+            root.preview to SkipType.Preview
+        ).map { (list, type) ->
+            list.map { stamp -> SkipStamp(type, stamp.startMs, stamp.endMs) }
+        }.flatten()
+    }
+
+    data class Root(
+        @JsonProperty("tmdb_id")
+        val tmdbId: Long,
+        @JsonProperty("type")
+        val type: String,
+        @JsonProperty("intro")
+        val intro: List<Stamp> = emptyList(),
+        @JsonProperty("recap")
+        val recap: List<Stamp> = emptyList(),
+        @JsonProperty("credits")
+        val credits: List<Stamp> = emptyList(),
+        @JsonProperty("preview")
+        val preview: List<Stamp> = emptyList(),
+    )
+
+    data class Stamp(
+        @JsonProperty("start_ms")
+        val startMs: Long,
+        @JsonProperty("end_ms")
+        val endMs: Long,
+    )
+}
