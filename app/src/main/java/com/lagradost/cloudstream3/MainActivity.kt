554fd3156cd70b5dd8c21bb13a1197ca8f60a848
Michele Granato <michele.granato985@gmail.com>
2026-04-27 19:22:26 +0200

    fix

--- app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
@@ -85,7 +85,7 @@ import com.lagradost.cloudstream3.mvvm.logError
 import com.lagradost.cloudstream3.mvvm.safe
 import com.lagradost.cloudstream3.mvvm.observe
 import com.lagradost.cloudstream3.mvvm.observeNullable
-
+import com.lagradost.cloudstream3.network.initClient
 import com.lagradost.cloudstream3.plugins.PluginManager
 import com.lagradost.cloudstream3.plugins.PluginManager.___DO_NOT_CALL_FROM_A_PLUGIN_loadAllOnlinePlugins
 import com.lagradost.cloudstream3.plugins.PluginManager.loadSinglePlugin
