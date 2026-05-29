950c1d2e395662cd67caa983a1c011c62dfe456f
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-29 13:26:12 -0600

    -

--- app/src/main/java/com/lagradost/cloudstream3/utils/InAppUpdater.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/InAppUpdater.kt
@@ -80,7 +80,7 @@ object InAppUpdater {
         return try {
             when {
                 // No updates on debug version
-                BuildConfig.DEBUG -> getPreReleaseUpdate()
+                BuildConfig.DEBUG -> Update(false, null, null, null, null)
                 BuildConfig.FLAVOR == "prerelease" || installPrerelease -> getPreReleaseUpdate()
                 else -> getReleaseUpdate()
             }
