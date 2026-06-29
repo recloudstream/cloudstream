0de8c35e8a2b6cdf94584cb9741e8d009c065fd9
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-06-28 19:58:02 -0600

    EmturbovidExtractor: replace selectXpath
    This will be necessary when we finally fully migrate to ksoup, which currently doesn't have selectXpath, but for our usages that is not necessary. This way is also more consistent with how we do it everywhere else.

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/EmturbovidExtractor.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/EmturbovidExtractor.kt
@@ -13,30 +13,27 @@ open class EmturbovidExtractor : ExtractorApi() {
     override val requiresReferer = false
 
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
-        val response = app.get(
-            url, referer = referer ?: "$mainUrl/"
-        )
-        val playerScript =
-            response.document.selectXpath("//script[contains(text(),'var urlPlay')]")
-                .html()
+        val response = app.get(url, referer = referer ?: "$mainUrl/")
+        val playerScript = response.document
+            .select("script:contains(var urlPlay)")
+            .html()
 
         val sources = mutableListOf<ExtractorLink>()
         if (playerScript.isNotBlank()) {
-            val m3u8Url =
-                playerScript.substringAfter("var urlPlay = '").substringBefore("'")
-
+            val m3u8Url = playerScript.substringAfter("var urlPlay = '").substringBefore("'")
             sources.add(
                 newExtractorLink(
                     source = name,
                     name = name,
                     url = m3u8Url,
-                    type = ExtractorLinkType.M3U8
+                    type = ExtractorLinkType.M3U8,
                 ) {
                     this.referer = "$mainUrl/"
                     this.quality = Qualities.Unknown.value
                 }
             )
         }
+
         return sources
     }
 }
