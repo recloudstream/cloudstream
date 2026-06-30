5a14a4410286f7b7933b0c7dd470560e15c69e22
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2026-06-30 17:24:28 +0530

    fix

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
@@ -1761,7 +1761,7 @@ class GeneratorPlayer : FullScreenPlayer() {
 
             is ExtractorUri -> {
                 headerName = meta.headerName
-                subName = if(meta.name != context?.getString(R.string.downloaded_file)) meta.name else ""
+                subName = meta.name
                 episode = meta.episode
                 season = meta.season
                 tvType = meta.tvType
