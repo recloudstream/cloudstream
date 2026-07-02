b5e2a0eff41856cde86ef1e14e5ff861875eecea
firelight <147925818+fire-light42@users.noreply.github.com>
2026-07-02 23:43:39 +0000

    Update user agent
    Fixes #2951

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainAPI.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainAPI.kt
@@ -89,7 +89,7 @@ annotation class SkipSerializationTest
 const val AllLanguagesName = "universal"
 
 const val USER_AGENT =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
 
 class ErrorLoadingException(message: String? = null) : Exception(message)
 
