daa399f3c3a97d6661748930a22431723997d1bc
kraptor123 <kraptor121@gmail.com>
2026-07-06 22:08:07 +0300

    short-code: formatting

--- app/src/main/java/com/lagradost/cloudstream3/plugins/RepositoryManager.kt
+++ app/src/main/java/com/lagradost/cloudstream3/plugins/RepositoryManager.kt
@@ -141,15 +141,16 @@ object RepositoryManager {
             }
         } else if (fixedUrl.matches("^[a-zA-Z0-9!_-]+$".toRegex())) {
             safeAsync {
-                if (fixedUrl.startsWith("!")){
-                    app.get("https://py.md/${fixedUrl.removePrefix("!")}", allowRedirects = false).let { it2 ->
-                        it2.headers["Location"]?.let { url ->
-                            if (url.startsWith("https://py.md/404")) return@safeAsync null
-                            if (url.removeSuffix("/") == "https://py.md") return@safeAsync null
-                            return@safeAsync url
+                if (fixedUrl.startsWith("!")) {
+                    app.get("https://py.md/${fixedUrl.removePrefix("!")}", allowRedirects = false)
+                        .let { it2 ->
+                            it2.headers["Location"]?.let { url ->
+                                if (url.startsWith("https://py.md/404")) return@safeAsync null
+                                if (url.removeSuffix("/") == "https://py.md") return@safeAsync null
+                                return@safeAsync url
+                            }
                         }
-                    }
-                }else {
+                } else {
                     app.get("https://cutt.ly/${fixedUrl}", allowRedirects = false).let { it2 ->
                         it2.headers["Location"]?.let { url ->
                             if (url.startsWith("https://cutt.ly/404")) return@safeAsync null
