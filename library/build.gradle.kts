d6bb2b21e7c9acbfe03a3b1d0508dded6a03623e
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-11 16:24:28 -0600

    Remove

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -88,8 +88,7 @@ kotlin {
     @OptIn(org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation::class)
     // https://kotlinlang.org/docs/gradle-binary-compatibility-validation.html
     abiValidation {
-        enabled.set(true)
-        this.filters {
+        filters {
             exclude {
                 annotatedWith.add("com.lagradost.cloudstream3.Prerelease")
                 annotatedWith.add("com.lagradost.cloudstream3.InternalAPI")
