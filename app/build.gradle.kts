3ee3acf44e94e1f94eaa6d1dc74595ef3e885cae
Spring <sdlcspring@gmail.com>
2026-08-25 00:07:35 +0700

    feat(ui): add preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -246,6 +246,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.activity.compose)
